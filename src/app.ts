42cd095835bd7e87f59dd63007c302d2cec72c97
Rabindra Tamang <rabindra.tamang@g-mana.com>
2025-02-16 00:03:40 +0545

    Add draggable

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,13 @@
 interface Draggable {
     dragStartHandler(event: DragEvent): void;
     dragEndHandler(event: DragEvent): void;
-  }
+}
   
-  interface DragTarget {
+interface DragTarget {
     dragOverHandler(event: DragEvent): void;
     dropHandler(event: DragEvent): void;
     dragLeaveHandler(event: DragEvent): void;
-  }
+}
   
   // Project Type
   enum ProjectStatus {
